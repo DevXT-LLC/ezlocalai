985763566c8f3380a945bfe826b3ab41ed7517e7
Josh XT <josh@devxt.com>
2023-10-02 19:09:30 -0400

    Set to none as default

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 COPY requirements.txt ./
 RUN pip install --upgrade pip && pip install -r requirements.txt
 COPY . .
-ARG MODEL_URL=""
+ARG MODEL_URL="None"
 ARG QUANT_TYPE="Q4_K_M"
 RUN python3 GetModel.py --model_url ${MODEL_URL} --quant_type ${QUANT_TYPE}
 EXPOSE 8091
--- GetModel.py
+++ GetModel.py
@@ -41,11 +41,11 @@ def get_model(model_url="TheBloke/Mistral-7B-OpenOrca-GGUF", quant_type="Q4_K_M"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_url", type=str, default="")
+    parser.add_argument("--model_url", type=str, default="None")
     parser.add_argument("--quant_type", type=str, default="Q4_K_M")
     args = parser.parse_args()
     model_url = args.model_url
     quant_type = args.quant_type
-    if model_url != "":
+    if model_url != "None":
         model_path = get_model(model_url, quant_type)
         print(model_path)
