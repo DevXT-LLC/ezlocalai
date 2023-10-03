79bef1051730913a94c331b67cff025a939f9412
Josh XT <josh@devxt.com>
2023-10-03 15:08:20 -0400

    Make server on launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,6 @@ BATCH_SIZE=${BATCH_SIZE:-512}
 
 # Workers should be threads divide by threads_batch rounded up to the nearest whole number
 UVICORN_WORKERS=$(python3 -c "from math import ceil; print(ceil($THREADS / $THREADS_BATCH))")
+make
 uvicorn app:app --host 0.0.0.0 --port 8091 --workers $UVICORN_WORKERS --proxy-headers &
 ./server -m $MODEL_PATH -c $MAX_TOKENS -ngl $GPU_LAYERS -t $THREADS -tb $THREADS_BATCH -mg $MAIN_GPU -b $BATCH_SIZE
