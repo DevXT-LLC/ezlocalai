b73338f632ddbcfbf00b03b53adca088aa1a4ff7
Josh XT <josh@devxt.com>
2023-10-02 22:51:26 -0400

    More auto config

--- configure.py
+++ configure.py
@@ -21,6 +21,18 @@ except ImportError:
     import GPUtil
 
 
+async def get_model_url(prompt):
+    model_response = await Gpt4freeProvider().instruct(prompt=prompt)
+    # Strip out anything that isn't the url from the response
+    extract_model_url = model_response.split("https://huggingface.co/")[1].split(
+        ".gguf"
+    )[0]
+    if not extract_model_url:
+        return await get_model_url(prompt)
+    new_model_url = f"https://huggingface.co/{extract_model_url}.gguf"
+    return new_model_url
+
+
 async def auto_configure(model_url="TheBloke/Mistral-7B-OpenOrca-GGUF"):
     readme = get_readme(model_url)
     table = readme.split("Provided files\n")[1].split("\n\n")[0]
@@ -38,32 +50,42 @@ async def auto_configure(model_url="TheBloke/Mistral-7B-OpenOrca-GGUF"):
     prompt += f"- CPU: {cpu_name} MHz\n"
     prompt += f"- RAM: {ram} MB\n\n"
     get_model_prompt = f"{prompt}**Determine the best model to run for a machine with the computer specifications provided. Ensure that the model will not utilize more than 80% of the resources. Return only the model URL of the best one.**"
-    model_response = await Gpt4freeProvider().instruct(prompt=get_model_prompt)
-    # Strip out anything that isn't the url from the response
-    extract_model_url = model_response.split("https://huggingface.co/")[1].split(
-        ".gguf"
-    )[0]
-    model_url = f"https://huggingface.co/{extract_model_url}.gguf"
+    new_model_url = get_model_url(get_model_prompt)
     # How many GPU layers, threads, and batch size?
     get_settings_prompt = f"{prompt}The default batch size is 512. The default GPU layers is set to 0. If not running an NVIDIA GPU, GPU Layers should be 0. **How many GPU layers, CPU threads, and batch size should we use when running llama.cpp on this computer?** Respond like this: `layers: 0, threads: 4, batch size: 512`"
     settings_response = await Gpt4freeProvider().instruct(prompt=get_settings_prompt)
     # Strip out anything that isn't the settings from the response
     gpu_layers = settings_response.split("layers: ")[1].split(", threads: ")[0]
+    if not gpu_layers:
+        gpu_layers = 0
     cpu_threads = settings_response.split(", threads: ")[1].split(", batch size: ")[0]
+    if not cpu_threads:
+        cpu_threads = 4
     batch_size = settings_response.split(", batch size: ")[1]
+    if not batch_size:
+        batch_size = 512
     get_max_tokens = f"{prompt}**Does anything indicate what the token limit is? Something like 8k, 16k, 32k, something like that would tell us.  If so, just respond with a python code block with the number.  For example, 8k would be 8192.**"
     max_tokens_response = await Gpt4freeProvider().instruct(prompt=get_max_tokens)
     # Strip out anything but numbers from max_tokens_response
     max_tokens = re.sub("[^0-9]", "", max_tokens_response)
     if not max_tokens:
         max_tokens = 8192
+    if max_tokens >= 32768:
+        # Run two 16k context models in parallel if over 32k tokens
+        threads_batch = cpu_threads / 2
+    else:
+        threads_batch = cpu_threads
     # Now we need to create a .env file with the settings
+    quantization_type = new_model_url.split(".Q")[1].split(".gguf")[0]
     with open(".env", "w") as f:
         f.write(f"GPU_LAYERS={gpu_layers}\n")
-        f.write(f"CPU_THREADS={cpu_threads}\n")
+        f.write(f"THREADS={cpu_threads}\n")
+        f.write(f"THREADS_BATCH={threads_batch}\n")
         f.write(f"BATCH_SIZE={batch_size}\n")
         f.write(f"MODEL_URL={model_url}\n")
         f.write(f"MAX_TOKENS={max_tokens}\n")
+        f.write(f"QUANT_TYPE=Q{quantization_type}\n")
+        f.write(f"MAIN_GPU=0")
     return model_url
 
 
