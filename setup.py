8019b4b613ac9786d0dd9d44bd4b4267056a6cc6
Josh XT <josh@devxt.com>
2023-10-04 15:14:17 -0400

    Add pypi publish

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(os.path.join(this_directory, "requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="local-llm",
+    version="0.0.3",
+    description="Local-LLM is a llama.cpp server in Docker with OpenAI Style Endpoints.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Josh XT",
+    author_email="josh@devxt.com",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=requirements,
+    include_dirs=["."],
+)
