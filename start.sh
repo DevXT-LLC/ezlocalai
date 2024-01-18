165d6f5dd48164ed7f93ed814e8d30e29c7f4229
Josh XT <josh@devxt.com>
2024-01-18 08:28:43 -0500

    Updates

--- start.sh
+++ start.sh
@@ -2,14 +2,7 @@
 
 set -e
 
-LOCAL_LLM_API_KEY=${LOCAL_LLM_API_KEY:-}
-THREADS=${THREADS:-($(nproc) - 1)}
 GPU_LAYERS=${GPU_LAYERS:-0}
-MAIN_GPU=${MAIN_GPU:-0}
-export LOCAL_LLM_API_KEY
-export THREADS
-export GPU_LAYERS
-export MAIN_GPU
 # if GPU_LAYERS are greater than 0, then we need to set CMAKES_ARGS to enable CUDA
 if [ $GPU_LAYERS -gt 0 ]; then
     CUDA_DOCKER_ARCH=all
