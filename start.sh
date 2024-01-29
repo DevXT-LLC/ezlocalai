0f69230b3bcfdda59f1b3ab07d6ce2d51dacf457
Josh XT <josh@devxt.com>
2024-01-29 06:36:06 -0500

    Use openblas

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@ set -e
 CUDA_DOCKER_ARCH=${CUDA_DOCKER_ARCH}
 if [ "$CUDA_DOCKER_ARCH" = "all" ]; then
     CUDA_DOCKER_ARCH=all
-    LLAMA_CUBLAS=1
-    CMAKE_ARGS="-DLLAMA_CUBLAS=on"
+    LLAMA_BLAS_VENDOR=OpenBLAS
+    LLAMA_BLAS=ON
+    CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS"
     export CUDA_DOCKER_ARCH
-    export LLAMA_CUBLAS
+    export LLAMA_BLAS_VENDOR
+    export LLAMA_BLAS
     export CMAKE_ARGS
-    CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python --force-reinstall
+    CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS" pip install llama-cpp-python --force-reinstall
 fi
 uvicorn app:app --host 0.0.0.0 --port 8091 --workers 1 --proxy-headers
