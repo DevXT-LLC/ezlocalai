9f4745d0a5bb9ddfb08cea1b1ae655a7c2eceb48
Josh XT <josh@devxt.com>
2024-02-27 10:06:09 -0500

    Use cu118 for cuda

--- linux-deps.sh
+++
@@ -1,8 +0,0 @@
-apt-get update --fix-missing && apt-get upgrade -y
-apt-get install -y --fix-missing --no-install-recommends git build-essential cmake gcc g++ portaudio19-dev ffmpeg libportaudio2 libasound-dev python3 python3-pip wget ocl-icd-opencl-dev opencl-headers clinfo libclblast-dev libopenblas-dev ninja-build python3.10-dev
-mkdir -p /etc/OpenCL/vendors && echo "libnvidia-opencl.so.1" > /etc/OpenCL/vendors/nvidia.icd
-ln -s /usr/bin/python3 /usr/bin/python
-apt-get clean && rm -rf /var/lib/apt/lists/* /var/cache/apt/* /tmp/* /var/tmp/*
-python3 -m pip install --upgrade pip cmake scikit-build setuptools wheel pyngrok --no-cache-dir
-pip install diffusers[torch] --no-cache-dir
-CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir
