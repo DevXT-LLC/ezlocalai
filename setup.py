2fc4f7c75099b3aedbd94eea9ce6d4c84f74d9ea
Josh XT <102809327+Josh-XT@users.noreply.github.com>
2026-02-21 09:05:26 -0500

    Bump version from 1.0.5 to 1.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "requirements.txt")) as f:
 
 setup(
     name="ezlocalai",
-    version="1.0.5",
+    version="1.0.6",
     description="ezlocalai is an easy to set up local multimodal artificial intelligence server with OpenAI Style Endpoints.",
     long_description=long_description,
     long_description_content_type="text/markdown",
